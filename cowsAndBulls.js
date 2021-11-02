df71909cc0e6b3790bd4c8d3a08d428a318bc4d0
Jon K <Johnakao@gmail.com>
2021-11-02 13:21:19 -0700

    Handles duplicate inputs

--- cowsAndBulls.js
+++ cowsAndBulls.js
@@ -1,1 +1,24 @@
-const getHint = (secret, guess) => {};
+const getHint = (secret, guess) => {
+  let hashmap = {};
+  let cows = 0,
+    bulls = 0;
+  for (let i = 0; i < secret.length; i++) {
+    hashmap[secret[i]] ? hashmap[secret[i]]++ : (hashmap[secret[i]] = 1);
+  }
+  for (let i = 0; i < guess.length; i++) {
+    let char = guess[i];
+    if (char in hashmap) {
+      if (char === secret[i]) {
+        bulls++;
+        if (hashmap[char] <= 0) cows--;
+      } else {
+        if (hashmap[char] > 0) cows++;
+      }
+    }
+    hashmap[char]--;
+  }
+  return `${bulls}A${cows}B`;
+};
+
+console.log(getHint("ABCD", "DBAC"));
+// "1A3B"
